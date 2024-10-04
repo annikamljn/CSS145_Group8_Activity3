aef8292dd7a446aa5c9274472abde15b2fabf4ac
Annika <impanniking@gmail.com>
2024-10-04 14:55:06 +0800

    Added github link

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import altair as alt
+import networkx as nx
+import plotly.graph_objects as go
+from io import StringIO
 
-st.title("My streamlit app ")
+st.title("Group #8, Section BM3")
+st.markdown("""
+    - BUNAG, Annika (2023102813, @annikamljn)
+    - CHUA, Denrick Ronn
+    - MALLILLIN, Loragene
+    - SIBAYAN, Gian Eugene
+    - UMALI, Ralph Dwayne
+"""
+)
+
+st.markdown("Click [here](https://github.com/annikamljn/CSS145_Group8_Activity3) to be redirected to our project's GitHub repository.")
+
